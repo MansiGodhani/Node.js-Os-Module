129caf52036da1e174271d1c1d0a4364f93faffe
MansiGodhani <81357299+MansiGodhani@users.noreply.github.com>
2021-11-26 10:55:01 +0530

    Add files via upload

---
+++ OsModule.js
@@ -0,0 +1,7 @@
+const os = require('os');
+
+console.log("Os CPU Architecture :",os.arch());
+console.log("Free memory of the system :",os.freemem());
+console.log("Total memory of the sytem :",os.totalmem());
+console.log("Os Platform :",os.platform());
+console.log("Information about the current user :",os.userInfo());
